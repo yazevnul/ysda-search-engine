faa56a2a8ebd9aa5555b70ad4f3a9217ec07c43a
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-02-23 18:32:15 +0300

    Sort document ids when building binary index.

--- src/tools/index_parser/main.cpp
+++ src/tools/index_parser/main.cpp
@@ -1,21 +1,39 @@
 #include <library/index/index.pb.h>
 #include <library/index/index.h>
 
+#include <algorithm>
 #include <cassert>
 #include <cstdint>
 #include <fstream>
 #include <istream>
 #include <sstream>
 #include <string>
+#include <tuple>
 #include <vector>
+
+
+struct DocumentIdWithWordFrequency {
+    yindex::DocumentId document_id = 0;
+    uint64_t word_frequency = 0;
+};
+
+
+inline bool operator <(
+    const DocumentIdWithWordFrequency& lhs, const DocumentIdWithWordFrequency& rhs
+){
+    return std::tie(lhs.document_id, lhs.word_frequency)
+           < std::tie(rhs.document_id, rhs.word_frequency);
+}
 
 
 yindex::Index ParseIndex(std::istream& input) {
     yindex::Index index;
 
+    std::vector<DocumentIdWithWordFrequency> documentIdsWithWordFrequencies;
     yindex::AssociatedDocuments entry;
     for (std::string line; std::getline(input, line);) {
         entry.Clear();
+        documentIdsWithWordFrequencies.clear();
         std::stringstream line_parser(line);
 
         yindex::WordId word_id = 0;
@@ -23,12 +41,20 @@ yindex::Index ParseIndex(std::istream& input) {
 
         assert(index.find(word_id) == index.end());
 
-        yindex::DocumentId document_id = 0;
-        uint64_t frequency = 0;
-        for (char colon; line_parser >> document_id >> colon >> frequency;) {
+        DocumentIdWithWordFrequency documentIdWithWordFrequency;
+        for (char colon;
+                line_parser >> documentIdWithWordFrequency.document_id
+                            >> colon
+                            >> documentIdWithWordFrequency.word_frequency;) {
+            assert(':' == colon);
+            documentIdsWithWordFrequencies.push_back(documentIdWithWordFrequency);
+        }
+
+        std::sort(documentIdsWithWordFrequencies.begin(), documentIdsWithWordFrequencies.end());
+        for (const auto& value: documentIdsWithWordFrequencies) {
             auto document_entry = entry.add_documents();
-            document_entry->set_document_id(document_id);
-            document_entry->set_word_frequency(frequency);
+            document_entry->set_document_id(value.document_id);
+            document_entry->set_word_frequency(value.word_frequency);
         }
 
         index[word_id] = entry;
