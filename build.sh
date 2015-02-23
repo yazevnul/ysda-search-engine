249d1d865eeafd3427cfa5a5817177da63c7946f
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-02-24 01:25:18 +0300

    Script to build all tools.

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${DIR}/src/tools/index_parser"
+./build.sh release
+
+cd "${DIR}/src/tools/dictionary_parser"
+./build.sh release
+
+cd "${DIR}/src/tools/ranker"
+./build.sh release
