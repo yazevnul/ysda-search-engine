b760d05ac9ccbf1727a87df2349764cef3bc8736
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-02-24 01:35:03 +0300

    Add protoc invocations to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd "${DIR}/src/library/index"
+protoc --cpp_out .  index.proto
+protoc --cpp_out .  dictionary.proto
+
 cd "${DIR}/src/tools/index_parser"
 ./build.sh release
 
