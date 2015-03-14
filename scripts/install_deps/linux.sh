eab049a47ec4b33bd5442d8b0766a6f909a68fa9
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-03-14 16:24:39 +0300

    yes.

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get install -y gcc-4.8
 
