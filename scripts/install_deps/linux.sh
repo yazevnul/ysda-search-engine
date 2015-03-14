99529762f1b35a22299bf828c77fa97ce20c1fe6
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-03-14 16:52:37 +0300

    fix

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -5,7 +5,7 @@ if [ ${CXX} == 'g++' ]; then
     sudo apt-get update -qq
     sudo apt-get install -y gcc-4.8
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-elif [ ${GXX} == 'clang++' ]; then
+elif [ ${CXX} == 'clang++' ]; then
     sudo deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main
     sudo deb-src http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
