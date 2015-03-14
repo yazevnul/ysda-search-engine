745e29a7d80eead423b5e05ff724a9e6abafedde
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-03-14 17:11:58 +0300

    fix

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -12,7 +12,7 @@ if [ ${CXX} == 'g++' ]; then
     g++ --version
 elif [ ${CXX} == 'clang++' ]; then
     sudo apt-get update -qq
-    sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
+    sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update -qq
     sudo apt-get install -y clang-3.5
