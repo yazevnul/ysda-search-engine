0b250615fa7b555ed0b08703e2cb44d9711f34ad
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-03-14 17:28:30 +0300

    fix

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -1,22 +1,32 @@
 #!/bin/bash -x
 
-if [ ${CXX} == 'g++' ]; then
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-get update -qq
-    sudo apt-get install -y gcc-4.8 g++-4.8
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
-    sudo update-alternatives --config gcc
-    sudo update-alternatives --config g++
+## GCC-4.8 installation BEGIN
 
-    g++ --version
-elif [ ${CXX} == 'clang++' ]; then
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+sudo apt-get install -y gcc-4.8 g++-4.8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
+
+g++ --version
+
+## GCC-4.8 installation END
+
+if [ ${CXX} == 'clang++' ]; then
     sudo apt-get update -qq
     sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update -qq
-    sudo apt-get install -y clang-3.5
-    sudo apt-get install -f -y
+    sudo apt-get install -y     \
+        clang-3.5               \
+        libclang-3.5            \
+        libllvm-3.5             \
+        libclang-common-3.5-dev \
+        llvm-3.5-dev            \
+        libgcc-4.8-dev          \
+        libobjc-4.8-dev
 fi
 
 sudo apt-get install -y \
