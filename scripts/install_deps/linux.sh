4399753f0bfe3cfb6e8af0426a521d54dee79ba2
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-03-14 18:14:21 +0300

    fix

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -14,19 +14,21 @@ g++ --version
 ## GCC-4.8 installation END
 
 if [ ${CXX} == 'clang++' ]; then
+    which clang++
+
     sudo apt-get update -qq
     sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update -qq
     sudo apt-get install -y     \
         clang-3.5               \
-        libclang-3.5            \
-        libllvm-3.5             \
+        libclang-3.5-dev        \
+        libllvm-3.5-ocaml-dev   \
         libclang-common-3.5-dev \
         llvm-3.5-dev            \
-        libgcc-4.8-dev          \
         libobjc-4.8-dev         \
         libc++1
+    sudo apt-get autoremove -y libgeos-3.2.2
     sudo update-alternatives --install /usr/bin/clang clang /usr/lib/llvm-3.5/bin/clang 20
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/lib/llvm-3.5/bin/clang++ 20
 
