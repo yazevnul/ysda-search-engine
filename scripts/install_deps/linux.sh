2a66a9fd9d1b0731c5c4de60a2380c1b6ccfbc29
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-04-14 00:56:16 +0300

    GCC-4.9 -> GCC-5

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -x
 
-## GCC-4.8 installation BEGIN
+## GCC-5 installation BEGIN
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-sudo apt-get install -y gcc-4.9 g++-4.9
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 20
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 20
+sudo apt-get install -y gcc-5 g++-5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 20
 
 which g++
 g++ --version
 
-## GCC-4.8 installation END
+## GCC-5 installation END
 
 if [ ${CXX} == 'clang++' ]; then
     sudo apt-get update -qq
