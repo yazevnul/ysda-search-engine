810b846f21de998e1229f2dbfbe335d59ff9db33
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-04-13 12:49:15 +0300

    3.5 -> 3.6

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -15,19 +15,19 @@ g++ --version
 
 if [ ${CXX} == 'clang++' ]; then
     sudo apt-get update -qq
-    sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
+    sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main'
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update -qq
     sudo apt-get install -y     \
-        clang-3.5               \
-        libclang-3.5-dev        \
-        libllvm-3.5-ocaml-dev   \
-        libclang-common-3.5-dev \
-        llvm-3.5-dev            \
+        clang-3.6               \
+        libclang-3.6-dev        \
+        libllvm-3.6-ocaml-dev   \
+        libclang-common-3.6-dev \
+        llvm-3.6-dev            \
         libobjc-4.8-dev
     sudo apt-get autoremove -y libgeos-3.2.2
-    sudo update-alternatives --install /usr/bin/clang clang /usr/lib/llvm-3.5/bin/clang 20
-    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/lib/llvm-3.5/bin/clang++ 20
+    sudo update-alternatives --install /usr/bin/clang clang /usr/lib/llvm-3.6/bin/clang 20
+    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/lib/llvm-3.6/bin/clang++ 20
 
     sudo rm /usr/local/clang-3.4/bin/clang++
 
