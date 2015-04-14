f577ce16c96e2beb8d436b03af1877e9a21f58ff
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-04-15 00:04:58 +0300

    roll back

--- scripts/install_deps/linux.sh
+++ scripts/install_deps/linux.sh
@@ -1,34 +1,31 @@
 #!/bin/bash -x
 
-## GCC-5 installation BEGIN
+## GCC-4.9 installation BEGIN
 
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-sudo apt-get install --yes \
-    g++-5                  \
-    gcc-5
-sudo apt-get install --fix-broken --yes
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 20
+sudo apt-get install -y gcc-4.9 g++-4.9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 20
 
 which g++
 g++ --version
 
-## GCC-5 installation END
+## GCC-4.9 installation END
 
 if [ ${CXX} == 'clang++' ]; then
     sudo apt-get update -qq
-    sudo add-apt-repository --yes 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main'
-    wget --output-document - 'http://llvm.org/apt/llvm-snapshot.gpg.key' | sudo apt-key add -
+    sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main'
+    wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update -qq
-    sudo apt-get install --yes     \
-        clang-3.6                  \
-        libclang-3.6-dev           \
-        libllvm-3.6-ocaml-dev      \
-        libclang-common-3.6-dev    \
-        llvm-3.6-dev               \
+    sudo apt-get install -y     \
+        clang-3.6               \
+        libclang-3.6-dev        \
+        libllvm-3.6-ocaml-dev   \
+        libclang-common-3.6-dev \
+        llvm-3.6-dev            \
         libobjc-4.8-dev
-    sudo apt-get autoremove --yes libgeos-3.2.2
+    sudo apt-get autoremove -y libgeos-3.2.2
     sudo update-alternatives --install /usr/bin/clang clang /usr/lib/llvm-3.6/bin/clang 20
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/lib/llvm-3.6/bin/clang++ 20
 
@@ -38,6 +35,6 @@ if [ ${CXX} == 'clang++' ]; then
     clang++ --version
 fi
 
-sudo apt-get install --yes \
-    protobuf-compiler      \
+sudo apt-get install -y \
+    protobuf-compiler   \
     libprotobuf-dev
