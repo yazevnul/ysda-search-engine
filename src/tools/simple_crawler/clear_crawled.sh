c46fcaef7f72282372c2221affaa1d3b62476556
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-04-21 20:57:01 +0300

    Useful scripts.

---
+++ src/tools/simple_crawler/clear_crawled.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+rm docs/* & \
+    rm docs_data/* & \
+    rm simple_crawler_state/*
---
+++ src/tools/simple_crawler/prepare_for_crawling.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+mkdir docs & \
+    mkdir docs_data & \
+    mkdir simple_crawler_state
