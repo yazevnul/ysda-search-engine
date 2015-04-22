3b52492350a55a78d0d4b0041d1b4c0f9d12d9b0
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-04-23 01:19:43 +0300

    make sugar work

--- src/library/logger/interface.h
+++ src/library/logger/interface.h
@@ -8,10 +8,10 @@
 namespace ylogger {
 
     enum Level : std::uint32_t {
-        INFO = 0,
-        DEBUG = 1,
-        ERROR = 2,
-        CRITICAL = 3
+        INFO_LVL = 0,
+        DEBUG_LVL = 1,
+        ERROR_LVL = 2,
+        CRITICAL_LVL = 3
     };
 
 
@@ -30,24 +30,22 @@ namespace ylogger {
 
 }  // namespace ylogger
 
-#define LIBRARY_LOGGER_PREFIX                      \
-    std::string{__FILE__} + ':' + __LINE__ + '\t'
 
 #define LOG_INFO(logger_ptr, message)                   \
-    logger_ptr->Info(LIBRARY_LOGGER_PREFIX + message)
+    logger_ptr->Info(std::string{__FILE__} + ':' + std::to_string(__LINE__) + '\t' + message)
 
 #define LOG_DEBUG(logger_ptr, message)                   \
-    logger_ptr->Debug(LIBRARY_LOGGER_PREFIX + message)
+    logger_ptr->Debug(std::string{__FILE__} + ':' + std::to_string(__LINE__) + '\t' + message)
 
 #define LOG_ERROR(logger_ptr, message)                   \
-    logger_ptr->Error(LIBRARY_LOGGER_PREFIX + message)
+    logger_ptr->Error(std::string{__FILE__} + ':' + std::to_string(__LINE__) + '\t' + message)
 
 #define LOG_CRITICAL(logger_ptr, message)                   \
-    logger_ptr->Critical(LIBRARY_LOGGER_PREFIX + message)
+    logger_ptr->Critical(std::string{__FILE__} + ':' + std::to_string(__LINE__) + '\t' + message)
 
 #undef LIBRARY_LOGGER_PREFIX
 
-#ifdef NDEBUG
+#ifdef DEBUG
 #define LOG_IF_DEBUG(logger_ptr, message) \
     LOG_DEBUG(logger_ptr, message)
 #else
